4f9080ea9770c7abbac1e48cc0d635c9db4b3f26
Raghav Gupta <2001guptar@gmail.com>
2021-11-12 00:10:14 -0800

    retry attempt 3

--- research/pages/server.js
+++ research/pages/server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// const mongoose = require('mongoose');
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-mongoose.connect('mongodb+srv://rg:gup@questiondata.wbq3q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+// mongoose.connect('')
 
-app.listen(4000, function() {
-    console.log("server is running on port 4000")
-})
+// app.listen(4000, function() {
+//     console.log("server is running on port 4000")
+// })
 
